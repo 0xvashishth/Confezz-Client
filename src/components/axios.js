78764b5e7c100ef74fa9d40608de16eba0648214
Vashishth Patel <vashishthchaudhary48@gmail.com>
2023-05-02 16:48:49 +0530

    changes done

--- src/components/axios.js
+++ src/components/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://Confezz-Server.vashishth-patel.repl.co',
+  baseURL: 'https://confezz-server.onrender.com/',
 });
 
 export default instance;
